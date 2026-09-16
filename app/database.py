88b122f700b0e3502d613b72169f40d9fbdff5dd
ohadd83 <ohad306@gmail.com>
2026-09-16 09:01:33 +0000

    add new products

--- app/database.py
+++ app/database.py
@@ -17,6 +17,18 @@ products_db = [
         "price": 40.00,
         "stock": 50
     }
+    {   
+        "id": 4,
+        "name": "MobilePhone",
+        "price": 1200.00,
+        "stock": 3
+    },
+    {
+        "id": 5,
+        "name": "TV"
+        "price": 2500.00,
+        "stock": 5
+    }
 ]
 
 
